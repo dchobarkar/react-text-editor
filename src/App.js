c52c59ba6b197c34aafbd40b995fed2a33514ddb
Darshan Chobarkar <dchobarkar@gmail.com>
2024-01-19 13:30:23 +0530

    add toolbar options

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
+const modules = {
+  toolbar: [
+    [{ font: [] }],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ color: [] }, { background: [] }],
+    [{ script: "sub" }, { script: "super" }],
+    ["blockquote", "code-block"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ indent: "-1" }, { indent: "+1" }, { align: [] }],
+    ["link", "image", "video"],
+    ["clean"],
+  ],
+};
+
 const App = () => {
-  return <ReactQuill theme="snow" />;
+  const [value, setValue] = useState("");
+  console.log(value);
+
+  return (
+    <ReactQuill
+      modules={modules}
+      theme="snow"
+      placeholder="Content goes here..."
+      onChange={setValue}
+    />
+  );
 };
 
 export default App;
